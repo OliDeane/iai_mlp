af28e08fe750f78c816e3c8199bb02f4c2bddddd
OliDeane <oliver.deane@gmail.com>
2020-12-08 11:46:25 +0000

    Added a tree classifier and a txt file that reports its performance forvarious parameter settings

---
+++ concat_stations.py
@@ -0,0 +1,21 @@
+"""
+Concatenates Stations
+"""
+
+import os
+import pandas as pd
+import glob
+
+def concat_files():
+    """returns concatenated stations"""
+    path = os.getcwd()
+    path = os.path.join(path, "morebikes2020/Train/Train")
+    all_files = glob.glob(path + "/*.csv")
+    li = []
+    for filename in all_files:
+        df = pd.read_csv(filename, index_col=None, header=0)
+        li.append(df)
+    
+    frame = pd.concat(li, axis=0, ignore_index=True)
+    return frame
+
